7d23512a1e5b9aef2648e3b84d8c6e9078588322
eunjee <silverhan38@gmail.com>
2023-07-07 10:07:21 +0900

    [Device Driver] First Commit

---
+++ DeviceDriver-Test/DeviceDriver_test.cpp
@@ -0,0 +1,9 @@
+#include <gmock/gmock.h>
+#include <gtest/gtest.h>
+#include "../DeviceDriver/DeviceDriver.cpp"
+using namespace testing;
+
+TEST(TestCaseName, TestName) {
+  EXPECT_EQ(1, 1);
+  EXPECT_TRUE(true);
+}
